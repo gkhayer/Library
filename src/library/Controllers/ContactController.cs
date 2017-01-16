334ab207b1694d2eae4a30787c67c4fded23ebbe
Gagan K. Hayer <ghayer@iteamnm.com>
2017-01-16 09:01:40 -0700

    Add project files.

---
+++ src/library/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace library.Controllers
+{
+   
+    public class ContactController : Controller
+    {
+       
+        public IActionResult Index()
+        {
+
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/library/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace library.Controllers
+{
+
+    public class HomeController : Controller
+    {
+    
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "This the your home page";
+            return View();
+        }
+    }
+}
